c88695e48bca5e695511c8d648f267ed1decdb48
charlseo <charseo@gmail.com>
2017-01-31 22:35:06 +1100

    - Optimising performance

--- ReqIFModuleLink/src/com/rcs/ap/ReqRelationFactory.java
+++ ReqIFModuleLink/src/com/rcs/ap/ReqRelationFactory.java
@@ -30,56 +30,98 @@ public class ReqRelationFactory {
 		
 		
 		System.out.println("----------------------");
-		for (int temp = 0; temp < requirements.size(); temp++) {
-			String reqCoreRef = requirements.get(temp).getCoreRef();
-			String reqRef = requirements.get(temp).getRef();
-			for (int i = 0; i < nlist.getLength(); i++) {
-				
-				Node nNode = nlist.item(i);
-				//System.out.println("\nCurrent element: " + nNode.getNodeName());
-				
-				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-					Element relation = (Element) nNode;
-					String lastChange =  relation.getAttribute("LAST-CHANGE");
-					String identifier =  relation.getAttribute("IDENTIFIER");
-					
-					NodeList source = relation.getElementsByTagName("SOURCE");
-					NodeList target = relation.getElementsByTagName("TARGET");
-					NodeList type = relation.getElementsByTagName("TYPE");
-					
-					Element sourceRef = (Element) source.item(0);
-					Element targetRef = (Element) target.item(0);
-					Element typeRef = (Element) type.item(0);
-					
-					String sourceRefCoreID = sourceRef.getElementsByTagName("SPEC-OBJECT-REF").item(0).getTextContent();
-					String targetRefCoreID = targetRef.getElementsByTagName("SPEC-OBJECT-REF").item(0).getTextContent();
-					String typeRefID = typeRef.getElementsByTagName("SPEC-RELATION-TYPE-REF").item(0).getTextContent();
-					
-					if (reqCoreRef.equals(sourceRefCoreID)) {
-						System.out.println("Source: " + sourceRefCoreID);
-						System.out.println("Target: " + targetRefCoreID);
-						ReqRelation reqRelation = new ReqRelation();
-						reqRelation.setSourceCoreReqID(sourceRefCoreID);
-						reqRelation.setTargetCoreReqID(targetRefCoreID);
-						reqRelation.setLastChange(lastChange);
-						reqRelation.setIdentifier(identifier);
-						reqRelation.setType(typeRefID);
-						reqRelation.setSourceReqID(reqRef);
-						reqLinks.add(reqRelation);
-						
-					}
-					
-
-
-
-				}
-			}
+//		for (int temp = 0; temp < requirements.size(); temp++) {
+//			String reqCoreRef = requirements.get(temp).getCoreRef();
+//			String reqRef = requirements.get(temp).getRef();
+//			for (int i = 0; i < nlist.getLength(); i++) {
+//				
+//				Node nNode = nlist.item(i);
+//				//System.out.println("\nCurrent element: " + nNode.getNodeName());
+//				
+//				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+//					Element relation = (Element) nNode;
+//					String lastChange =  relation.getAttribute("LAST-CHANGE");
+//					String identifier =  relation.getAttribute("IDENTIFIER");
+//					
+//					NodeList source = relation.getElementsByTagName("SOURCE");
+//					NodeList target = relation.getElementsByTagName("TARGET");
+//					NodeList type = relation.getElementsByTagName("TYPE");
+//					
+//					Element sourceRef = (Element) source.item(0);
+//					Element targetRef = (Element) target.item(0);
+//					Element typeRef = (Element) type.item(0);
+//					
+//					String sourceRefCoreID = sourceRef.getElementsByTagName("SPEC-OBJECT-REF").item(0).getTextContent();
+//					String targetRefCoreID = targetRef.getElementsByTagName("SPEC-OBJECT-REF").item(0).getTextContent();
+//					String typeRefID = typeRef.getElementsByTagName("SPEC-RELATION-TYPE-REF").item(0).getTextContent();
+//					
+//					if (reqCoreRef.equals(sourceRefCoreID)) {
+//						System.out.println("Source: " + sourceRefCoreID);
+//						System.out.println("Target: " + targetRefCoreID);
+//						ReqRelation reqRelation = new ReqRelation();
+//						reqRelation.setSourceCoreReqID(sourceRefCoreID);
+//						reqRelation.setTargetCoreReqID(targetRefCoreID);
+//						reqRelation.setLastChange(lastChange);
+//						reqRelation.setIdentifier(identifier);
+//						reqRelation.setType(typeRefID);
+//						reqRelation.setSourceReqID(reqRef);
+//						reqLinks.add(reqRelation);
+//						
+//					}
+//				}
+//			}
 			
+//		}
+		
+		for (int i = 0; i < nlist.getLength(); i++) {
+			
+			Node nNode = nlist.item(i);
+			//System.out.println("\nCurrent element: " + nNode.getNodeName());
+			
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+				Element relation = (Element) nNode;
+				String lastChange =  relation.getAttribute("LAST-CHANGE");
+				String identifier =  relation.getAttribute("IDENTIFIER");
+				
+				NodeList source = relation.getElementsByTagName("SOURCE");
+				NodeList target = relation.getElementsByTagName("TARGET");
+				NodeList type = relation.getElementsByTagName("TYPE");
+				
+				Element sourceRef = (Element) source.item(0);
+				Element targetRef = (Element) target.item(0);
+				Element typeRef = (Element) type.item(0);
+				
+				String sourceRefCoreID = sourceRef.getElementsByTagName("SPEC-OBJECT-REF").item(0).getTextContent();
+				String targetRefCoreID = targetRef.getElementsByTagName("SPEC-OBJECT-REF").item(0).getTextContent();
+				String typeRefID = typeRef.getElementsByTagName("SPEC-RELATION-TYPE-REF").item(0).getTextContent();
+				System.out.println("Source: " + sourceRefCoreID);
+				System.out.println("Target: " + targetRefCoreID);
+				ReqRelation reqRelation = new ReqRelation();
+				reqRelation.setSourceCoreReqID(sourceRefCoreID);
+				reqRelation.setTargetCoreReqID(targetRefCoreID);
+				reqRelation.setLastChange(lastChange);
+				reqRelation.setIdentifier(identifier);
+				reqRelation.setType(typeRefID);
+				//reqRelation.setSourceReqID(reqRef);
+				reqLinks.add(reqRelation);
+			}
 		}
 		
+		addReqRefIntoReqRelation();
+	}
+	
+	private void addReqRefIntoReqRelation(){
 		
-		
-		
+		for (int i = 0; i < requirements.size(); i++) {
+			String reqCoreRef = requirements.get(i).getCoreRef();
+			String reqRef = requirements.get(i).getRef();
+			for (int j = 0; j < reqLinks.size(); j++) {
+				String reqLinkSourceCoreID = reqLinks.get(j).getSourceCoreReqID();
+				if (reqLinkSourceCoreID.equals(reqCoreRef)){
+					reqLinks.get(j).setSourceReqID(reqRef);
+				}
+			}
+		}
 	}
 
 	public ArrayList<ReqRelation> getReqLinks() {
